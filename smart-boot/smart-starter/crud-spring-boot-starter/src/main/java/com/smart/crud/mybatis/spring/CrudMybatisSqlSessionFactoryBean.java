592177cc9ddbade9192931b4cfe5c6532adbf94a
zhongming.shi <zhongming4762@hotmail.com>
2022-06-08 18:30:47 +0800

    Revert "typeEnumsPackages 同时支持 MybatisEnumPackageProvider设置和CrudMybatisSqlSessionFactoryBean设置"
    This reverts commit 6d598185e97a19cd164a4507bf609204b0e27fe2.

--- smart-boot/smart-starter/crud-spring-boot-starter/src/main/java/com/smart/crud/mybatis/spring/CrudMybatisSqlSessionFactoryBean.java
+++ smart-boot/smart-starter/crud-spring-boot-starter/src/main/java/com/smart/crud/mybatis/spring/CrudMybatisSqlSessionFactoryBean.java
@@ -1,6 +1,8 @@
 package com.smart.crud.mybatis.spring;
 
 import com.baomidou.mybatisplus.extension.spring.MybatisSqlSessionFactoryBean;
+import lombok.Setter;
+import org.apache.ibatis.session.SqlSessionFactory;
 
 import java.util.Collection;
 import java.util.HashSet;
@@ -13,7 +15,8 @@ import java.util.Set;
  */
 public class CrudMybatisSqlSessionFactoryBean extends MybatisSqlSessionFactoryBean {
 
-    private final Set<String> typeEnumsPackages;
+    @Setter
+    private Set<String> typeEnumsPackages;
 
     public CrudMybatisSqlSessionFactoryBean() {
         this.typeEnumsPackages = new HashSet<>();
@@ -25,10 +28,6 @@ public class CrudMybatisSqlSessionFactoryBean extends MybatisSqlSessionFactoryBe
 
     public void addEnumPackage(Collection<String> enumsPackages) {
         this.typeEnumsPackages.addAll(enumsPackages);
-    }
-
-    public void setTypeEnumsPackages(Set<String> typeEnumsPackages) {
-        this.addEnumPackage(typeEnumsPackages);
     }
 
     @Override
