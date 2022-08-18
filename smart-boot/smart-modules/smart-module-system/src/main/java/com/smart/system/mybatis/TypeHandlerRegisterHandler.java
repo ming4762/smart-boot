f0cd4ce7cc06667923225cecce088b39204e0499
zhongming4762 <zhongming4762@hotmail.com>
2022-08-18 10:53:44 +0800

    健壮性：系统用户类型使用枚举类
    (cherry picked from commit cb722fac8b04a40298d9427b60f0cbdfb95c32ed)

---
+++ smart-boot/smart-modules/smart-module-system/src/main/java/com/smart/system/mybatis/TypeHandlerRegisterHandler.java
@@ -0,0 +1,39 @@
+package com.smart.system.mybatis;
+
+import com.smart.system.mybatis.type.*;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.type.TypeHandler;
+import org.apache.ibatis.type.TypeHandlerRegistry;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * @author ShiZhongMing
+ * 2022/8/18
+ * @since 3.0.0
+ */
+@Component
+public class TypeHandlerRegisterHandler implements InitializingBean {
+
+    private static final List<Class<? extends TypeHandler<?>>> TYPE_HANDLER_CLASS = List.of(
+            FunctionTypeTypeHandler.class,
+            LogSourceTypeHandler.class,
+            MaxConnectionsPolicyTypeHandler.class,
+            UserAccountStatusTypeHandler.class,
+            UserTypeTypeHandler.class
+    );
+
+    private final SqlSessionFactory sqlSessionFactory;
+
+    public TypeHandlerRegisterHandler(SqlSessionFactory sqlSessionFactory) {
+        this.sqlSessionFactory = sqlSessionFactory;
+    }
+
+    @Override
+    public void afterPropertiesSet() {
+        TypeHandlerRegistry registry = this.sqlSessionFactory.getConfiguration().getTypeHandlerRegistry();
+        TYPE_HANDLER_CLASS.forEach(registry::register);
+    }
+}
