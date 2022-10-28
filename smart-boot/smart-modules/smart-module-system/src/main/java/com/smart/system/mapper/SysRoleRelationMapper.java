ccbcac6ab945dd907600e6d7b22453d40a8b7083
zhongming4762 <zhongming4762@hotmail.com>
2022-10-28 17:07:56 +0800

    添加角色关系表

---
+++ smart-boot/smart-modules/smart-module-system/src/main/java/com/smart/system/mapper/SysRoleRelationMapper.java
@@ -0,0 +1,12 @@
+package com.smart.system.mapper;
+
+import com.smart.crud.mapper.CrudBaseMapper;
+import com.smart.system.model.SysRoleRelationPO;
+
+/**
+ * @author ShiZhongMing
+ * 2022/10/28
+ * @since 3.0.0
+ */
+public interface SysRoleRelationMapper extends CrudBaseMapper<SysRoleRelationPO> {
+}
---
+++ smart-boot/smart-modules/smart-module-system/src/main/java/com/smart/system/service/SysRoleRelationService.java
@@ -0,0 +1,12 @@
+package com.smart.system.service;
+
+import com.smart.crud.service.BaseService;
+import com.smart.system.model.SysRoleRelationPO;
+
+/**
+ * @author ShiZhongMing
+ * 2022/10/28
+ * @since 3.0.0
+ */
+public interface SysRoleRelationService extends BaseService<SysRoleRelationPO> {
+}
