b6cdb8bef9b4ca40f73c7b6312b8ac1c08796e92
zhongming4762 <zhongming4762@hotmail.com>
2022-10-28 13:20:24 +0800

    UI添加可拖拽放大 modal，基于ant-design-vue modal开发

---
+++ smart-ui/src/components/modal/ModalDrag.vue
@@ -0,0 +1,61 @@
+<template>
+  <a-modal
+    class="modal-diag"
+    :get-container="getContainer"
+    v-bind='$attrs'>
+    <template #closeIcon />
+    <template #title>
+      <slot></slot>
+      <div class="top-button-container">
+        <a-button class="button">
+          <template #icon>
+            <fullscreen-outlined />
+          </template>
+        </a-button>
+        <a-button class="button">
+          <CloseOutlined />
+        </a-button>
+      </div>
+    </template>
+    这是一个示例
+  </a-modal>
+</template>
+
+<script lang='ts' setup>
+/**
+ * 可拖拽的modal
+ */
+import type { PropType } from 'vue'
+
+import { FullscreenOutlined, FullscreenExitOutlined, CloseOutlined } from '@ant-design/icons-vue'
+
+const props = defineProps({
+  title: {
+    type: String as PropType<string>
+  }
+})
+
+/**
+ * 获取挂载点
+ */
+const getContainer = () => document.getElementById('modal_container')
+</script>
+
+<style lang='less'>
+.top-button-container {
+  position: absolute;
+  right: 0;
+  top: 0;
+  button {
+    border: 0;
+    height: 54px;
+    padding: 0;
+    width: 35px;
+  }
+}
+.modal-container {
+  .ant-modal-close {
+    display: none;
+  }
+}
+</style>
