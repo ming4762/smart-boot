84da329a0dbf9b52845cedc6357c76fe0fb432c1
ming4762 <zhongming4762@hotmail.com>
2022-12-11 19:34:49 +0800

    添加登录用户页面和接口日志页面
    (cherry picked from commit a0db8d48b766f890a6d4902f243d6082215eef43)

---
+++ smart-ui-vben/src/modules/system/views/interfaceLog/InterfaceLogListHooks.ts
@@ -0,0 +1,36 @@
+import { ref } from 'vue'
+
+import ApiService from '/@/common/utils/ApiService'
+
+/**
+ * 显示日志详情
+ */
+export const useShowDetails = () => {
+  const detailsModalVisible = ref(false)
+  // 加载状态
+  const getDetailLoading = ref(false)
+  const detailsData = ref<any>({})
+
+  const handleShowDetails = async (id: number) => {
+    detailsModalVisible.value = true
+    getDetailLoading.value = true
+    try {
+      detailsData.value = await ApiService.postAjax('sys/log/getById', id)
+    } finally {
+      getDetailLoading.value = false
+    }
+  }
+  /**
+   * 执行关闭操作
+   */
+  const handleCloseDetails = () => {
+    detailsModalVisible.value = false
+  }
+  return {
+    detailsModalVisible,
+    handleShowDetails,
+    getDetailLoading,
+    detailsData,
+    handleCloseDetails,
+  }
+}
