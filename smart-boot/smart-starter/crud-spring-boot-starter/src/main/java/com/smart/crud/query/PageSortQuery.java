21879f86c38af5b9c2aae363a5f17b28b5b07590
ming4762 <zhongming4762@hotmail.com>
2023-01-14 18:15:53 +0800

    crud启动器添加分页参数

--- smart-boot/smart-starter/crud-spring-boot-starter/src/main/java/com/smart/crud/query/PageSortQuery.java
+++ smart-boot/smart-starter/crud-spring-boot-starter/src/main/java/com/smart/crud/query/PageSortQuery.java
@@ -5,6 +5,8 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Optional;
+
 /**
  * 分页排序查询
  * @author shizhongming
@@ -34,8 +36,14 @@ public class PageSortQuery extends CommonQuery {
     /**
      * 每页条数
      */
-    @Schema(title = "每页记录数")
+    @Schema(title = "每页记录数，优先级高")
     private Integer limit;
+
+    /**
+     * 每页条数
+     */
+    @Schema(title = "每页记录数，优先级低")
+    private Integer pageSize;
 
     /**
      * 起始记录数
@@ -49,9 +57,22 @@ public class PageSortQuery extends CommonQuery {
     @Schema(title = "当前页数，page优先")
     private Integer page;
 
+    @Schema(title = "当前页数，page优先")
+    private Integer currentPage;
+
     /**
      * 关键字查询
      */
     @Schema(title = "关键字查询")
     private String keyword;
+
+    public Integer getLimit() {
+        return Optional.ofNullable(this.limit)
+                .orElse(this.pageSize);
+    }
+
+    public Integer getPage() {
+        return Optional.ofNullable(this.page)
+                .orElse(this.currentPage);
+    }
 }
