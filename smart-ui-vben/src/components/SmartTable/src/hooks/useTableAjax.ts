fe02b182b4e9d1158a33c805719b899083e5dbb5
zhongming4762 <zhongming4762@hotmail.com>
2023-02-08 21:11:16 +0800

    bug fix：smart-table修复表格autoload初次加载后无法处理 after-load事件

--- smart-ui-vben/src/components/SmartTable/src/hooks/useTableAjax.ts
+++ smart-ui-vben/src/components/SmartTable/src/hooks/useTableAjax.ts
@@ -29,10 +29,11 @@ export const useTableAjax = (
 ) => {
   const { t } = useI18n()
   // 是否自动加载，取消vxe-table自动加载，解决触发事件问题（事件无法在数据加载完成之后执行）
-  onMounted(() => {
+  onMounted(async () => {
     const proxyConfig = unref(propsRef).proxyConfig
     if (proxyConfig && proxyConfig.autoLoad !== false) {
-      commitVxeProxy('_init')
+      await commitVxeProxy('_init')
+      emit('after-load')
     }
   })
 
