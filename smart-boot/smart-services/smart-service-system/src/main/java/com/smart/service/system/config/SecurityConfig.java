1a69188f0264f07b25328c7204d6fdf1dd3cc1f4
zhongming4762 <zhongming4762@hotmail.com>
2023-03-28 17:55:18 +0800

    feat(项目):添加系统模块启动服务

---
+++ smart-boot/smart-services/smart-service-system/src/main/java/com/smart/service/system/config/SecurityConfig.java
@@ -0,0 +1,53 @@
+package com.smart.service.system.config;
+
+import com.smart.auth.core.properties.AuthProperties;
+import com.smart.auth.extensions.jwt.AuthJwtSecurityConfigurer;
+import com.smart.auth.security.config.AuthCaptchaSecurityConfigurer;
+import com.smart.auth.security.config.AuthWebSecurityConfigurerAdapter;
+import lombok.SneakyThrows;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * @author shizhongming
+ * 2021/4/25 10:05 下午
+ */
+@Configuration(proxyBeanMethods = false)
+@EnableWebSecurity
+public class SecurityConfig extends AuthWebSecurityConfigurerAdapter {
+
+    public SecurityConfig(AuthProperties authProperties) {
+        super(authProperties);
+    }
+
+    @SneakyThrows(Exception.class)
+    @Bean
+    public SecurityFilterChain securityFilterChainConfig(HttpSecurity http, ApplicationContext applicationContext) {
+        super.configure(http);
+        http
+                .formLogin().disable()
+                .logout().disable()
+                .httpBasic().disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                // jwt配置
+                .apply(AuthJwtSecurityConfigurer.jwt())
+                .serviceProvider()
+                .applicationContext(applicationContext)
+                .and()
+                .and()
+                // 验证码配置
+                .apply(AuthCaptchaSecurityConfigurer.captcha())
+                .serviceProvider()
+                .applicationContext(applicationContext)
+                .loginUrl(AuthJwtSecurityConfigurer.LOGIN_URL);
+        return http.build();
+    }
+
+}
