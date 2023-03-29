bd4b19299b94ef3c0a8d14ac943cfb8b236e4dbe
zhongming4762 <zhongming4762@hotmail.com>
2023-03-29 19:57:59 +0800

    fix(文件管理):修复文件管理页面查询报错的问题
    (cherry picked from commit 0bcbef169422e73da5ef60a3883d63b2f1c6d671)

--- smart-ui-vben/src/modules/fileManager/views/SmartFile/SmartFileListView.config.ts
+++ smart-ui-vben/src/modules/fileManager/views/SmartFile/SmartFileListView.config.ts
@@ -97,7 +97,7 @@ export const getFormSchemas = (t: Function): FormSchema[] => {
       label: t('system.views.file.title.fileStorageId'),
       component: 'SmartApiSelectTable',
       componentProps: {
-        modelClassName: 'com.smart.system.model.file.SmartFileStoragePO',
+        modelClassName: 'com.smart.file.manager.model.SmartFileStoragePO',
         valueFieldName: 'id',
         labelFieldName: 'storageName',
         params: {
@@ -164,7 +164,7 @@ export const getSearchFormSchemas = (t: Function): SmartSearchFormSchema[] => {
         style: {
           width: '150px',
         },
-        modelClassName: 'com.smart.system.model.file.SmartFileStoragePO',
+        modelClassName: 'com.smart.file.manager.model.SmartFileStoragePO',
         valueFieldName: 'id',
         labelFieldName: 'storageName',
         params: {
