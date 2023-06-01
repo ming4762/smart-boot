ed742b4941c348286b872457da45fed2d9189bd9
zhongming4762 <zhongming4762@hotmail.com>
2023-06-01 09:18:57 +0800

    添加消息服务
    (cherry picked from commit e70c6609d009de4801038cfc3f49e8fd70ab9f10)

---
+++ smart-boot/smart-module-api/smart-message-api/src/main/java/com/smart/module/api/message/SmartMessageApi.java
@@ -0,0 +1,8 @@
+package com.smart.module.api.message;
+
+/**
+ * @author zhongming4762
+ * 2023/5/31 17:57
+ */
+public interface SmartMessageApi {
+}
---
+++ smart-boot/smart-module-api/smart-message-api/src/main/java/com/smart/module/api/message/constants/MessageChannelEnum.java
@@ -0,0 +1,31 @@
+package com.smart.module.api.message.constants;
+
+import lombok.Getter;
+
+/**
+ * 消息通道列表
+ * @author zhongming4762
+ * 2023/5/31
+ */
+@Getter
+public enum MessageChannelEnum {
+
+    /**
+     * 消息发送通道
+     */
+    SYSTEM("SYSTEM", "系统消息"),
+    SMS("SMS", "短信"),
+    EMAIL("EMAIL", "邮件消息"),
+    WECHAT("WECHAT", "微信消息"),
+    DINGDING("DINGDING", "钉钉消息"),
+    ;
+
+    private final String value;
+
+    private final String remark;
+
+    MessageChannelEnum(String value, String remark) {
+        this.value = value;
+        this.remark = remark;
+    }
+}
