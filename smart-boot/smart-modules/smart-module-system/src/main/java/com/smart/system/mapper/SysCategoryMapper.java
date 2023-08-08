11fb9f2ba7b83722bc9774680199f87215dfff19
zhongming4762 <zhongming4762@hotmail.com>
2023-08-08 19:58:01 +0800

    refactor(crud启动器):优化crud启动器，优化updateHasChild函数（使用case代替mysql函数 IF）

--- smart-boot/smart-modules/smart-module-system/src/main/java/com/smart/system/mapper/SysCategoryMapper.java
+++ smart-boot/smart-modules/smart-module-system/src/main/java/com/smart/system/mapper/SysCategoryMapper.java
@@ -2,7 +2,6 @@ package com.smart.system.mapper;
 
 import com.smart.crud.mapper.CrudBaseMapper;
 import com.smart.system.model.SysCategoryPO;
-import org.apache.ibatis.annotations.Param;
 
 /**
 * sys_category - 分类字段 mapper层
