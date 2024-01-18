57980b52602bb8f5d7b6d8f4903c4685c98af079
zhongming4762 <zhongming4762@hotmail.com>
2024-01-18 19:46:32 +0800

    feat(smart-table):smart-table功能完善

---
+++ smart-ui-vben/src/components/SmartTable/src/hooks/useSmartTableColumnSortStorage.ts
@@ -0,0 +1,4 @@
+import type { SmartColumn } from '/@/components/SmartTable'
+import type { ComputedRef } from 'vue'
+
+export const useSmartTableColumnSortStorage = (columns: ComputedRef<SmartColumn[]>) => {}
