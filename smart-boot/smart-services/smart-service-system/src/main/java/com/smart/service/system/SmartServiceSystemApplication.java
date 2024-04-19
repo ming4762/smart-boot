291a71ae093d24101e7d7d5270e0252609f3594d
zhongming4762 <zhongming4762@hotmail.com>
2024-04-19 10:15:57 +0800

    perf(启动类): 启动类添加租户支持

--- smart-boot/smart-services/smart-service-system/src/main/java/com/smart/service/system/SmartServiceSystemApplication.java
+++ smart-boot/smart-services/smart-service-system/src/main/java/com/smart/service/system/SmartServiceSystemApplication.java
@@ -5,10 +5,10 @@ import com.smart.commons.core.cors.EnableCors;
 import com.smart.commons.core.spring.EnableRateLimit;
 import com.smart.commons.core.spring.EnabledCustomObjectMapper;
 import com.smart.commons.core.validate.EnableGlobalValidator;
+import com.smart.crud.spring.EnableMybatisPlusTenant;
 import com.smart.i18n.config.EnableValidatorI18nSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.freemarker.FreeMarkerAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -16,7 +16,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
  * @author zhongming4762
  * 2023/3/28
  */
-@SpringBootApplication(exclude = { FreeMarkerAutoConfiguration.class })
+@SpringBootApplication
 @EnableTransactionManagement
 @EnableValidatorI18nSource
 @EnableCors
@@ -25,6 +25,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableRateLimit
 @EnableCaching
 @EnabledCustomObjectMapper
+@EnableMybatisPlusTenant
 public class SmartServiceSystemApplication {
 
     public static void main(String[] args) {
