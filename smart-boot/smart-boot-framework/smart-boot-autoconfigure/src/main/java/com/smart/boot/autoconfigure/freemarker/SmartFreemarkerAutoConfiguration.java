86d15bfdd25c9dda7a868919acc72be7f8f37012
zhongming4762 <zhongming4762@hotmail.com>
2025-01-01 19:28:24 +0800

    fix(build): 修复未引入freemarker导致启动报错的问题

--- smart-boot/smart-boot-framework/smart-boot-autoconfigure/src/main/java/com/smart/boot/autoconfigure/freemarker/SmartFreemarkerAutoConfiguration.java
+++ smart-boot/smart-boot-framework/smart-boot-autoconfigure/src/main/java/com/smart/boot/autoconfigure/freemarker/SmartFreemarkerAutoConfiguration.java
@@ -2,6 +2,7 @@ package com.smart.boot.autoconfigure.freemarker;
 
 import com.smart.framework.freemarker.engine.FreemarkerTemplateEngine;
 import freemarker.cache.ByteArrayTemplateLoader;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +14,7 @@ import org.springframework.context.annotation.Configuration;
  * @since 5.0.0
  */
 @Configuration(proxyBeanMethods = false)
+@ConditionalOnClass(FreemarkerTemplateEngine.class)
 public class SmartFreemarkerAutoConfiguration {
 
     @Bean
