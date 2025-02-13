6b14a1b12de934a8011af7d29b4b525c8d1cd8c7
zhongming4762 <zhongming4762@hotmail.com>
2025-02-13 15:53:10 +0800

    feat(smart-crud): 1、增加多租户支持 2、支持手动切换租户

--- smart-boot/smart-framework/smart-crud/src/main/java/com/smart/framework/crud/plus/tenant/SmartTenantControl.java
+++ smart-boot/smart-framework/smart-crud/src/main/java/com/smart/framework/crud/plus/tenant/SmartTenantControl.java
@@ -19,7 +19,7 @@ import java.util.concurrent.ConcurrentHashMap;
  * 2025/2/12 11:25
  * @since 5.0.0
  */
-public class SmartTenantControl {
+public final class SmartTenantControl {
 
     private SmartTenantControl() {
         throw new IllegalStateException("Utility class");
@@ -40,16 +40,24 @@ public class SmartTenantControl {
      * @param ignoreCommandList 忽略的命令
      * @param platformTenantIgnoreCommandList 平台管理租户忽略的命令
      */
-    public static void setIgnore(String tableName, List<SqlCommandType> ignoreCommandList, List<SqlCommandType> platformTenantIgnoreCommandList) {
+    public static void changeIgnore(String tableName, List<SqlCommandType> ignoreCommandList, List<SqlCommandType> platformTenantIgnoreCommandList) {
         THREAD_IGNORE_LOCAL.get().put(tableName, new SmartTenantIgnoreData(ignoreCommandList, tableName, platformTenantIgnoreCommandList));
     }
 
     /**
-     * 设置租户字段
+     * 重置忽略租户的信息
+     * @param tableName 表名
+     */
+    public static void restIgnore(String tableName) {
+        THREAD_IGNORE_LOCAL.get().remove(tableName);
+    }
+
+    /**
+     * 切换租户字段
      * @param tableName 表名
      * @param tenantField 租户字段
      */
-    public static void setTableTenantField(String tableName, String tenantField) {
+    public static void switchTenantField(String tableName, String tenantField) {
         SmartTableInfo smartTableInfo = CrudUtils.getTableInfo(tableName);
         if (smartTableInfo == null) {
             throw new SystemException("表名错误，未找到表信息");
@@ -62,11 +70,19 @@ public class SmartTenantControl {
     }
 
     /**
+     * 重置租户字段
+     * @param tableName 表名
+     */
+    public static void restTenantField(String tableName) {
+        THREAD_FIELD_LOCAL.get().remove(tableName);
+    }
+
+    /**
      * 获取租户字段
      * @param tableName 表名
      * @return 租户字段
      */
-    public static String getTableTenantField(String tableName) {
+    static String getTableTenantField(String tableName) {
         return THREAD_FIELD_LOCAL.get().get(tableName);
     }
 
@@ -75,7 +91,7 @@ public class SmartTenantControl {
      * @param tableName 表名
      * @return 忽略租户的信息
      */
-    public static SmartTenantIgnoreData getIgnore(String tableName) {
+    static SmartTenantIgnoreData getIgnore(String tableName) {
         return THREAD_IGNORE_LOCAL.get().get(tableName);
     }
 
