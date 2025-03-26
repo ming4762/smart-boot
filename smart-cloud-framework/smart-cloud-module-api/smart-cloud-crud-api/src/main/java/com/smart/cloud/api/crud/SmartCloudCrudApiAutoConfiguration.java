236ee2f972994f68b27211c9b77016d72522b8a7
zhongming4762 <zhongming4762@hotmail.com>
2025-03-26 16:16:22 +0800

    fix(crud): 修复类名注入错误

--- smart-cloud-framework/smart-cloud-module-api/smart-cloud-crud-api/src/main/java/com/smart/cloud/api/crud/SmartCloudCrudApiAutoConfiguration.java
+++ smart-cloud-framework/smart-cloud-module-api/smart-cloud-crud-api/src/main/java/com/smart/cloud/api/crud/SmartCloudCrudApiAutoConfiguration.java
@@ -14,7 +14,7 @@ import org.springframework.context.annotation.Configuration;
 public class SmartCloudCrudApiAutoConfiguration {
 
     @Bean
-    public SmartCrudUserCloudApi remoteSmartFileApi(SysUserApi sysUserApi) {
+    public SmartCrudUserCloudApi smartCrudUserCloudApi(SysUserApi sysUserApi) {
         return new SmartCrudUserCloudApi(sysUserApi);
     }
 
