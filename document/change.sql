3cc957fc02730ae54276d9a2efed97575ccac310
zhongming4762 <zhongming4762@hotmail.com>
2025-04-14 21:14:19 +0800

    feat(smart-file): 文件存储支持加密（目前只有阿里云OSS支持）

--- document/change.sql
+++ document/change.sql
@@ -9,3 +9,5 @@ alter table smart_file_storage
 alter table smart_file_storage
     add public_key text null comment '加密文件公钥' after private_key;
 
+alter table smart_file
+    add encrypted_yn tinyint(1) default 0 not null comment '文件是否加密，与存储器文件加密不同，这里可以单独指定每个文件是否加密，保证存储器这设置为加密之前的文件也可正常读取';
