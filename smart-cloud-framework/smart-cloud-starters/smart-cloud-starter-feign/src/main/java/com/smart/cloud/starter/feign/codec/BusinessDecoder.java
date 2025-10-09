f15b509b8af08c7b3fe53a83da053134fef2d83a
zhongming4762 <zhongming4762@hotmail.com>
2025-10-09 13:53:25 +0800

    perf(cloud-feign): 优化feign异常信息拦截

--- smart-cloud-framework/smart-cloud-starters/smart-cloud-starter-feign/src/main/java/com/smart/cloud/starter/feign/codec/BusinessDecoder.java
+++ smart-cloud-framework/smart-cloud-starters/smart-cloud-starter-feign/src/main/java/com/smart/cloud/starter/feign/codec/BusinessDecoder.java
@@ -30,7 +30,7 @@ public class BusinessDecoder extends ResponseEntityDecoder {
 
     private static final String CODE_KEY = "code";
     private static final String ERROR_CODE = "500";
-    private static final String MESSAGE_KEY = "message";
+    private static final String DATA_KEY = "data";
 
     public BusinessDecoder(Decoder decoder) {
         super(decoder);
@@ -57,7 +57,7 @@ public class BusinessDecoder extends ResponseEntityDecoder {
             if (ERROR_CODE.equals(code)) {
                 // 业务异常
                 Map<?,?> err = objectMapper.convertValue(root, Map.class);
-                throw new SmartFeignBusinessException(err.get(MESSAGE_KEY) == null ? null : err.get(MESSAGE_KEY).toString(), err);
+                throw new SmartFeignBusinessException(err.get(DATA_KEY) == null ? null : JsonUtils.toJsonString(err.get(DATA_KEY)), err);
             }
         }
 
