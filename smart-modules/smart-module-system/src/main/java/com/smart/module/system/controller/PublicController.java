a65c2f04553e2e1a0af51a1a878275b9eb273e44
zhongming4762 <zhongming4762@hotmail.com>
2026-02-28 18:28:35 +0800

    perf(smart-system): 完善I18n消息查询接口

--- smart-modules/smart-module-system/src/main/java/com/smart/module/system/controller/PublicController.java
+++ smart-modules/smart-module-system/src/main/java/com/smart/module/system/controller/PublicController.java
@@ -3,13 +3,18 @@ package com.smart.module.system.controller;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.smart.framework.commons.core.message.Result;
 import com.smart.module.system.constants.SysI18nPlatformEnum;
+import com.smart.module.system.pojo.parameter.i18n.SysI18nReadParameter;
 import com.smart.module.system.service.SysI18nJsonService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 /**
  * @author shizhongming
@@ -25,7 +30,11 @@ public class PublicController {
 
     @Operation(summary = "读取前台国际化信息")
     @PostMapping("i18n/readFrontI18n")
-    public Result<JsonNode> readFrontI18n() {
-        return Result.success(this.sysI18nJsonService.readToJsonByLocale(LocaleContextHolder.getLocale(), SysI18nPlatformEnum.FRONT));
+    public Result<JsonNode> readFrontI18n(@RequestBody SysI18nReadParameter parameter) {
+        Locale locale = LocaleContextHolder.getLocale();
+        if (StringUtils.hasText(parameter.getLocale())) {
+            locale = Locale.forLanguageTag(parameter.getLocale());
+        }
+        return Result.success(this.sysI18nJsonService.readToJsonByLocale(locale, SysI18nPlatformEnum.FRONT));
     }
 }
---
+++ smart-modules/smart-module-system/src/main/java/com/smart/module/system/pojo/parameter/i18n/SysI18nReadParameter.java
@@ -0,0 +1,19 @@
+package com.smart.module.system.pojo.parameter.i18n;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author <a href="https://github.com/ming4762">ShiZhongMing</a>
+ * 2026-02-26 13:54
+ * @since 5.0.0
+ */
+@Getter
+@Setter
+public class SysI18nReadParameter implements Serializable {
+
+    private String locale;
+}
